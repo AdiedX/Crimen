8c376ed48122dcae26c763db7bb0a2a58e434af4
Adi Sivasankaran <adi.sivasankaran@gmail.com>
2014-06-10 19:05:10 -0400

    The project has markers working (using the old school way) and the maps working as before (the Angular way). Next step is to implement the clustering algorithm.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,15 @@
+
+
 'use strict';
 
 angular.module('crimespaceAngularApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $http.get('/api/getCrimeData').success(function(crimeData) {
-        console.log(crimeData);
+  .controller('MainCtrl', function ($scope, $http)
+  {
+    $http.get('/api/getCrimeData').success(function(crimeData)
+    {
+        console.log(crimeData.length);
         $scope.crimeMarkers = crimeData;
+        // $scope.$apply();
     });
     $scope.map = {
             center: {
@@ -14,7 +19,64 @@ angular.module('crimespaceAngularApp')
             zoom: 8,
             options: {
                 mapTypeId: google.maps.MapTypeId.ROADMAP,
-                styles: [{"featureType":"water","stylers":[{"color":"#021019"}]},{"featureType":"landscape","stylers":[{"color":"#08304b"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#0c4152"},{"lightness":5}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#0b434f"},{"lightness":25}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#0b3d51"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"}]},{"elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"elementType":"labels.text.stroke","stylers":[{"color":"#000000"},{"lightness":13}]},{"featureType":"transit","stylers":[{"color":"#146474"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#144b53"},{"lightness":14},{"weight":1.4}]}]
+                styles: [{"featureType":"all","elementType":"all","stylers":[{"invert_lightness":true},{"saturation":10},{"lightness":30},{"gamma":0.5},{"hue":"#435158"}]}]
             }
         };
+
+    // SETTING UP MARKERS THE OLD SCHOOL WAY:
+    // Pass into Angular Google Maps:
+    $scope.mapControl = {};
+    // var str_title = element.type + ' ' + element.month + ', ' + element.year;
+    $scope.$watch('mapControl', function(mapControl){
+        var map = $scope.mapControl.getGMap();
+
+        $http.get('/api/getCrimeData').success(function(crimeData)
+        {
+            crimeData.forEach(function(element)
+            {
+                var myLatlng = new google.maps.LatLng(element.latitude, element.longitude);
+                var marker = new google.maps.Marker({
+
+                    position: myLatlng,
+                    map: map,
+                    title: element.type
+                });
+            });
+
+            // CLUSTERING ALGORITHM:
+            // var markers = [];
+            // for (var i = 0; i < 100; i++)
+            // {
+            //     var latLng = new google.maps.LatLng(data.photos[i].latitude, data.photos[i].longitude);
+            //     var marker = new google.maps.Marker({'position': latLng});
+            //     markers.push(marker);
+            // }
+            // var markerCluster = new MarkerClusterer(map, markers);
+            // // We specify a number of option to fine-tune the marker manager's performance:
+            // var mcOptions = {gridSize: 50, maxZoom: 15};
+
+            // // This is where a specific set of markers to be clustered go in:
+            // var markers = [];
+
+            // // For each instance of crime in the database:
+            // crimeData.forEach(function(crime)
+            // {
+            //     // Why this is wrong:
+            //     // I need to push markers from a specific geographic area
+            //     // This methods currently selects markers in order
+            //     markers.push(markers);
+            // });
+
+            // //
+            // var mc = new MarkerClusterer(map, markers, mc);
+        });
+    });
+
   });
+
+
+
+
+
+
+
