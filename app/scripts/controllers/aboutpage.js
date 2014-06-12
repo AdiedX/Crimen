40ca763209c686bf3b8198f99fc56cac550e120a
Adi Sivasankaran <adi.sivasankaran@gmail.com>
2014-06-12 18:49:37 -0400

    Fixed padding and resizing issues related to Google Maps.  Used Unsaturated Browsn for Snazzy Maps.  Next Steps: Set up About and Crime Statistics html pages.

---
+++ app/scripts/controllers/aboutpage.js
@@ -0,0 +1,10 @@
+
+
+'use strict';
+
+angular.module('crimespaceAngularApp')
+  .controller('AboutpageCtrl', function ($scope, $http) {
+    // $http.get('/api/').success(function(awesomeThings) {
+    //   $scope.awesomeThings = awesomeThings;
+    // });
+  });
---
+++ test/client/spec/controllers/aboutpage.js
@@ -0,0 +1,28 @@
+'use strict';
+
+describe('Controller: AboutpageCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('crimespaceAngularApp'));
+
+  var AboutpageCtrl,
+    scope,
+    $httpBackend;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
+    $httpBackend = _$httpBackend_;
+    $httpBackend.expectGET('/api/awesomeThings')
+      .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
+    scope = $rootScope.$new();
+    AboutpageCtrl = $controller('AboutpageCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('should attach a list of awesomeThings to the scope', function () {
+    expect(scope.awesomeThings).toBeUndefined();
+    $httpBackend.flush();
+    expect(scope.awesomeThings.length).toBe(4);
+  });
+});
